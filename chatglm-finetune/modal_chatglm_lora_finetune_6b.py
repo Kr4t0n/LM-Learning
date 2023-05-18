b6bab1ec8abbfb88d9c5c3dd63174f7b26b1e8f0
kyle.yang <kyle.yang1995@gmail.com>
2023-05-18 18:25:32 +0800

    MAINT: add a gradio app for inference

--- chatglm-finetune/modal_chatglm_lora_finetune_6b.py
+++ chatglm-finetune/modal_chatglm_lora_finetune_6b.py
@@ -4,7 +4,7 @@ stub = modal.Stub("modal-chatglm-lora-finetune-6b")
 image = modal.Image.debian_slim().pip_install(
     "torch", "sentencepiece", "wandb", "cpm_kernels",
     "bitsandbytes", "accelerate",
-    "transformers==4.27.1", "datasets", "peft"
+    "transformers==4.27.1", "datasets", "peft", "gradio"
 ).run_commands(
     # patch libbitsandbytes replace cpu so file with cuda one
     "cd /usr/local/lib/python3.10/site-packages/bitsandbytes/ && cp libbitsandbytes_cuda117.so libbitsandbytes_cpu.so",  # noqa
@@ -133,6 +133,97 @@ def chatglm_alpaca_lora_6b_infer():
             )
             out_text = tokenizer.decode(out[0])
             print(out_text)
+
+
+@stub.cls(
+    image=image,
+    shared_volumes={
+        "/root/.cache": model_cache_vol,
+        "/root/chatglm-finetune-output": model_finetune_vol
+    },
+    gpu="A10G"
+)
+class Model:
+
+    def __enter__(self):
+
+        from peft import PeftModel
+        from transformers import AutoTokenizer, AutoModel
+
+        model_name = "THUDM/chatglm-6b"
+
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name,
+            trust_remote_code=True
+        )
+        self.model = AutoModel.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+            device_map="auto"
+        ).half()
+        self.model = PeftModel.from_pretrained(
+            self.model,
+            "/root/chatglm-finetune-output/6b_max_steps_20000"
+        )
+
+    @modal.method()
+    def infer(self, input_text):
+
+        import torch
+
+        ids = self.tokenizer.encode(input_text)
+        input_ids = torch.LongTensor([ids]).to(self.model.device)
+
+        out = self.model.generate(
+            input_ids=input_ids,
+            max_length=150,
+            do_sample=False,
+            temperature=0
+        )
+        out_text = self.tokenizer.decode(out[0])
+
+        return out_text
+
+
+@stub.function(
+    image=image,
+    concurrency_limit=1
+)
+@modal.asgi_app()
+def fastapi_app():
+
+    import gradio as gr
+
+    from fastapi import FastAPI
+    from gradio.routes import mount_gradio_app
+
+    web_app = FastAPI()
+
+    def infer(text):
+        return Model().infer.call(text)
+
+    title = "ChatGLM-6B Finetuned"
+    description = "ChatGLM-6B Finetunes over Alapaca Dataset using LoRA"
+    examples = [
+        "Instruction: you have a 6-gallon cup and a 7-gallon cup, how do you measure 4 gallons of water?\nAnswer: ",  # noqa
+        "Instruction: you can solve math problem\nInput: you have a 6-gallon cup and a 7-gallon cup, how do you measure 4 gallons of water?\nAnswer: ",  # noqa
+        "Instruction: solve this math problem\nInput: you have a 6-gallon cup and a 7-gallon cup, how do you measure 4 gallons of water?\nAnswer: ",  # noqa
+        "Instruction: solve this math problem, use step by step thinking\nInput: you have a 6-gallon cup and a 7-gallon cup, how do you measure 4 gallons of water?\nAnswer: ",  # noqa
+    ]
+
+    interface = gr.Interface(
+        fn=infer,
+        inputs="text",
+        outputs="text",
+        title=title,
+        description=description,
+        examples=examples,
+        allow_flagging="never"
+    )
+
+    return mount_gradio_app(
+        app=web_app, blocks=interface, path='/'
+    )
 
 
 @stub.local_entrypoint()
